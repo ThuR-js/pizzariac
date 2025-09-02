b3ca0063d6aac1bd630a5233723055013e19b8d2
ThuR-js <rm94448@estudante.fieb.edu.br>
2025-09-02 08:19:04 -0300

    Configuração da conexão com o banco de dados

--- src/main/java/com/itb/inf2cm/pizzariac/model/entity/Usuario.java
+++ src/main/java/com/itb/inf2cm/pizzariac/model/entity/Usuario.java
@@ -5,6 +5,7 @@ public class Usuario {
     private Long id;
     private String nome;
     private String cpf;
+    private String tipoUsuario;
     private String email;
     private String senha;
     private String sexo;
@@ -37,6 +38,14 @@ public class Usuario {
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
     }
 
     public String getEmail() {
